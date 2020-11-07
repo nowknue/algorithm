7b7043af00139158ee6c43d5842f72ae77761807
HYUNGEUN KWON <nowknue@gmail.com>
2020-11-07 13:33:17 +0900

    study

---
+++ src/boj/Car.java
@@ -0,0 +1,24 @@
+package boj;
+
+public class Car {
+
+	String name;
+	int price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		
+		return super.equals(obj);
+	}
+}
