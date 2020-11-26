db9bd1c2b0c07ebce97004b2c54f45bb48b347af
HYUNGEUN KWON <nowknue@gmail.com>
2020-11-27 07:05:19 +0900

    study

--- src/boj/BOJ2357_st.java
+++ src/boj/BOJ2357_st.java
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ2357_st {
-	
+
 	static long[] arr, tree;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
