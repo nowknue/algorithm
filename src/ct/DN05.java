f0575187b68e206609c550fe631c6851b4d6aa8c
HYUNGEUN KWON <nowknue@gmail.com>
2020-12-08 22:41:29 +0900

    study

---
+++ src/ct/DN05.java
@@ -0,0 +1,23 @@
+package ct;
+
+public class DN05 {
+   
+   public static void main(String args[]) {
+      /*
+      SELECT
+          A.BRANCH_ID AS BRANCH_ID
+          , COUNT(B.CAR_ID) AS '계약 건수'
+      FROM
+          EMPLOYEES A
+          , SELLINGS B
+      WHERE
+          A.ID = B.EMPLOYEE_ID
+      GROUP BY
+          A.BRANCH_ID
+      ORDER BY
+          A.BRANCH_ID
+       
+       */
+   }
+
+}
