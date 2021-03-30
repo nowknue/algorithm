3182cd820d8d90c5eef15ed97859bc496d7fcf5e
HYUNGEUN KWON <nowknue@gmail.com>
2021-03-30 23:40:17 +0900

    study

---
+++ src/ct/GB04.java
@@ -0,0 +1,24 @@
+package ct;
+
+public class GB04 {
+
+   public static void main(String args[]) {
+      /*
+      SELECT
+         A.USER_ID
+      FROM
+         CARTS A
+         , CART_PRODUCTS B
+      WHERE
+         A.ID = B.CART_ID
+         AND B.NAME = 'Flour'
+      GROUP BY
+         A.USER_ID
+      ORDER BY
+         A.USER_ID
+       */
+      
+
+   }
+
+}
