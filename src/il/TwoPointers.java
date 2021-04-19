0ca627333bf09b5153d517c3f5859ac23354872e
HYUNGEUN KWON <nowknue@gmail.com>
2021-04-19 22:44:36 +0900

    study

--- src/il/TwoPointers.java
+++ src/il/TwoPointers.java
@@ -2,34 +2,34 @@ package il;
 
 public class TwoPointers {
 
-   public static void main(String[] args) {
-      TwoPointers a = new TwoPointers();
-      int n = 5;
-      int answer = a.solve(n);
-      System.out.println(answer);
-   }
+	public static void main(String[] args) {
+		TwoPointers a = new TwoPointers();
+		int n = 5;
+		int answer = a.solve(n);
+		System.out.println(answer);
+	}
 
-   public int solve(int n) {
-      int answer = 0;
-      int rt = 2;
-      int lt = 1;
-      
-      while (lt <= n/2) {
-         int sum = 0;
-         for (int i = lt; i < rt; i++) {
-            sum += i;
-         }
-         
-         if (sum > n) {
-            lt++;
-         } else if (sum < n) {
-            rt++;
-         } else {
-            answer++;
-            lt++;
-         }
-      }
-      
-      return answer;
-   }
+	public int solve(int n) {
+		int answer = 0;
+		int rt = 2;
+		int lt = 1;
+
+		while (lt <= n / 2) {
+			int sum = 0;
+			for (int i = lt; i < rt; i++) {
+				sum += i;
+			}
+
+			if (sum > n) {
+				lt++;
+			} else if (sum < n) {
+				rt++;
+			} else {
+				answer++;
+				lt++;
+			}
+		}
+
+		return answer;
+	}
 }
