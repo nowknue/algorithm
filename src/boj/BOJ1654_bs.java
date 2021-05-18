4ebd4f4a906197af5baf832ed289ddbb9e87938f
HYUNGEUN KWON <nowknue@gmail.com>
2021-05-18 23:17:22 +0900

    study

--- src/boj/BOJ1654_bs.java
+++ src/boj/BOJ1654_bs.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class BOJ1654_bs {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
