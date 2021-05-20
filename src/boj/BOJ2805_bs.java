7cfb29bb52b526c0067974e063fad0d2bd77fb7e
HYUNGEUN KWON <nowknue@gmail.com>
2021-05-20 23:43:41 +0900

    study

--- src/boj/BOJ2805_bs.java
+++ src/boj/BOJ2805_bs.java
@@ -7,45 +7,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ2805_bs {
-    private static int n, m;
-    private static long[] trees;
+	private static int n, m;
+	private static long[] trees;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-        trees = new long[n];
+		trees = new long[n];
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            trees[i] = Long.parseLong(st.nextToken());
-        }
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			trees[i] = Long.parseLong(st.nextToken());
+		}
 
-        Arrays.sort(trees);
+		Arrays.sort(trees);
 
-        long left = 1;
-        long right = trees[trees.length - 1];
+		long left = 1;
+		long right = trees[trees.length - 1];
 
-        while (left <= right) {
-            long mid = (left + right) / 2;
+		while (left <= right) {
+			long mid = (left + right) / 2;
 
-            long sum = 0;
-            for (long tree : trees) {
-                if (tree > mid) {
-                    sum += tree - mid;
-                }
-            }
+			long sum = 0;
+			for (long tree : trees) {
+				if (tree > mid) {
+					sum += tree - mid;
+				}
+			}
 
-            if (sum >= m) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+			if (sum >= m) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
 
-        System.out.println(right);
-    }
+		System.out.println(right);
+	}
 }
